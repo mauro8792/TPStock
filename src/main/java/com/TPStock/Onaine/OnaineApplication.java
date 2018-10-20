cf9dd5fd203e882711baa9730b0c9a59309308e1
MauroYini <mauro8792yini@gmail.com>
2018-10-20 20:34:04 -0300

    Primer commit

---
+++ src/main/java/com/TPStock/Onaine/OnaineApplication.java
@@ -0,0 +1,12 @@
+package com.TPStock.Onaine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnaineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnaineApplication.class, args);
+	}
+}
